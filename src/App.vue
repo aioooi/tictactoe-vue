5e1f6caacae0fb2f978e25c6a5684d860b2a13ed
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-07-09 10:51:45 +0200

    pass props

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <h1>Let's Play: Tic Tac Toe</h1>
     <h2>(implemented using Vue.js)</h2>
     <div id="game">
-      <Game />
+      <!-- prop playerBegins is referred to as 'player-begins' @vue...  -->
+      <Game :player-begins=true :delay=1000></Game>
     </div>
   </div>
 </template>
