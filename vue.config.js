03dbd66319e49267bea0a73f005de4f1da1244ac
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-07-11 17:04:20 +0200

    fix public path for deploy to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = 'Vue Tic Tac Toe';
-        return args;
-      })
-  }
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/tictactoe-vue/" : "/",
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Vue Tic Tac Toe";
+      return args;
+    });
+  },
+};
